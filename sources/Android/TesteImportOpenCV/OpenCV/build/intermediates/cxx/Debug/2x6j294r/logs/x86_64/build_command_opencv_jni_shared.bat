ad65685288dc0101037ff8f633a62d34d02f715e
gpribeiro-furb <pancabiel@gmail.com>
2024-03-19 19:21:04 -0300

    Import OpenCV funcionando

---
+++ sources/Android/TesteImportOpenCV/OpenCV/build/intermediates/cxx/Debug/2x6j294r/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\panca\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\panca\\Documents\\source\\repos\\TCCFurbot\\sources\\Android\\TesteImportOpenCV\\OpenCV\\.cxx\\Debug\\2x6j294r\\x86_64" ^
+  opencv_jni_shared
---
+++ sources/Android/TesteImportOpenCV/app/src/main/java/com/example/testeimportopencv/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.testeimportopencv;
+
+import static android.content.ContentValues.TAG;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (OpenCVLoader.initLocal()) {
+            Log.i(TAG, "OpenCV loaded successfully");
+        } else {
+            Log.e(TAG, "OpenCV initialization failed!");
+            (Toast.makeText(this, "OpenCV initialization failed!", Toast.LENGTH_LONG)).show();
+            return;
+        }
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
