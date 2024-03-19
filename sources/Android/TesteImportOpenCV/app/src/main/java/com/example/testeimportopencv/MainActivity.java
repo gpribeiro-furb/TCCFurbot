17955b2e3ffe26a7730c99ddab0ec9837541d096
gpribeiro-furb <pancabiel@gmail.com>
2024-03-19 19:55:18 -0300

    Testes não funcionando

--- sources/Android/TesteImportOpenCV/app/src/main/java/com/example/testeimportopencv/MainActivity.java
+++ sources/Android/TesteImportOpenCV/app/src/main/java/com/example/testeimportopencv/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.testeimportopencv;
 
-import static android.content.ContentValues.TAG;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -12,9 +12,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends CameraActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private Mat mRgba;
+
+    private static final String TAG = "TesteImportOpenCV::Activity";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private VideoCapture mVideoCapture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +38,60 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //! [keep_screen]
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //! [keep_screen]
+
+//        setContentView(R.layout.tutorial1_surface_view);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        mVideoCapture = new VideoCapture();
+        mVideoCapture.open("http://192.168.1.10:81/stream"); // Replace with your stream URL
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initDebug();
+        mOpenCvCameraView.enableView();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        if (mVideoCapture.isOpened()) {
+            mRgba = new Mat();
+            mVideoCapture.read(mRgba);
+            return mRgba;
+        } else {
+            return inputFrame.rgba();
+        }
     }
 }
